e6a3708f55251a725d0c6399315cc804a783f20d
Nimish <nimish.ie19@nsut.ac.in>
2023-09-28 20:38:32 +0530

    Data ingestion from SQL datbase done

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from src.housingproject.logger import logging
 from src.housingproject.exception import CustomException
+from src.housingproject.components.data_ingestion import DataIngestion
 import sys
 
 if __name__ =="__main__":
     logging.info("Logging started with main")
 
     try:
-        a=1/0
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
